d2b0358869341760955867711660b68575a50a16
zoo868e <zoo868e@gmail.com>
2022-10-11 16:32:21 +0800

    Add the interface header file

--- chatroom.cpp
+++ chatroom.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include"./include/connection_common.h"
+#include"./include/interface.h"
 using namespace std;
 int main(int argc, char* argv[]){
 	int server_fd, server_portno, pid;
